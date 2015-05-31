f3efcc8b9ef3d8d3ae1f073ccef41854baa43cd5
Bert Balcaen <bert@rekalldesign.com>
2015-05-31 12:57:33 +0200

    Testing shutdown + file delete script.

--- piratebox/piratebox/www/cgi-bin/shared_files.py
+++ piratebox/piratebox/www/cgi-bin/shared_files.py
@@ -1,5 +1,6 @@
 from os import listdir
 from os.path import isfile, join
+mypath = "/opt/piratebox/www/Shared/"
 files = [ f for f in listdir(mypath) if isfile(join(mypath,f)) ]
 for f in files:
     print f
